0a718d828f0343ee6978ec7ec693458b2b098ec9
3omar-mostafa <42312059+3omar-mostafa@users.noreply.github.com>
2022-10-31 16:02:09 +0100

    Properly Support Versions >= 10

--- extract.sh
+++ extract.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e -o pipefail
 
 wget -q 'https://www.memtest86.com/downloads/memtest86-usb.zip'
 sha512sum memtest86-usb.zip
@@ -8,36 +9,40 @@ unzip -q memtest86-usb.zip
 # Note: How to get the offset in the mount command
 # run this command: fdisk -lu memtest86-usb.img
 # ------------------------------------Output-------------------------------------
-# Disk memtest86-usb.img: 500 MiB, 524288000 bytes, 1024000 sectors
+# Disk memtest86-usb.img: 1 GiB, 1073741824 bytes, 2097152 sectors
 # Units: sectors of 1 * 512 = 512 bytes
 # Sector size (logical/physical): 512 bytes / 512 bytes
 # I/O size (minimum/optimal): 512 bytes / 512 bytes
 # Disklabel type: gpt
-# Disk identifier: 68264C0F-858A-49F0-B692-195B64BE4DD7
-# Device              Start     End Sectors  Size Type
-# memtest86-usb.img1   2048  512000  509953  249M Microsoft basic data
-# memtest86-usb.img2 514048 1023966  509919  249M EFI System
+# Disk identifier: 9A82C153-9542-42F5-8883-CF351C5DE568
+
+# Device               Start     End Sectors  Size Type
+# memtest86-usb.img1    2048  524287  522240  255M Microsoft basic data
+# memtest86-usb.img2  524288 1048575  524288  256M EFI System
+# memtest86-usb.img3 1048576 2097118 1048543  512M Microsoft basic data
 # -----------------------------------End Output-----------------------------------
 #
-# In the last line we can see that EFI System has start offset of 514048
+# In the output we can see that EFI System has start offset of 524288
 # But this is in Sectors not Bytes, We can see that Sector = 512
-# i.e. offset = 514048 * 512 = 263192576
+# i.e. offset = 524288 * 512 = 268435456
 # ==============================================================================
 
-# offset will be 263192576, but we calculate it dynamically in case it changed
-sector_offset=$(fdisk -lu memtest86-usb.img | grep EFI | cut -d' ' -f2)
+# In version 9, offset was 263192576
+# In version 10, offset is 268435456
+# We calculate it dynamically in case it changed
+sector_offset=$(fdisk -lu memtest86-usb.img | grep EFI | tr -s ' ' | cut -d' ' -f2)
 SECTOR_SIZE=512
 offset=$(( SECTOR_SIZE * sector_offset ))
 
-sudo mkdir -p /mnt/memtest
-sudo mount -o loop,offset=${offset} memtest86-usb.img /mnt/memtest/
+mkdir -p /mnt/memtest
+mount -o loop,offset=${offset} memtest86-usb.img /mnt/memtest/
 mkdir memtest_efi
-sudo cp -r /mnt/memtest/EFI/BOOT/* ./memtest_efi
-sudo umount /mnt/memtest/
+cp -r /mnt/memtest/EFI/BOOT/* ./memtest_efi
+umount /mnt/memtest/
 
 ls -lhR ./memtest_efi/
 cd memtest_efi
-sudo sha512sum * > checksum.sha512
+sha512sum * > checksum.sha512 || true
 cd ..
 
 zip -q memtest86_efi.zip memtest_efi/*
